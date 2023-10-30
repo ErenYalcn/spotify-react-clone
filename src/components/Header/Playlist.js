d5a5a1162bafd78d10723cd7cfc7a9daecf73ef2
Eren Yalçın <erenyalcn09@gmail.com>
2023-10-30 18:18:34 +0300

    change: names for playlist

--- src/components/Header/Playlist.js
+++ src/components/Header/Playlist.js
@@ -1,74 +1,106 @@
-import React from 'react';
+import React from "react";
 
 export default function Playlist() {
   return (
-    <nav className='flex'>
-        <ul>
-            <li className='mb-2'>
-                <a href="#" className='text-link hover:text-white text-sm font-semibold'>
-                    3. Çalma Listem
-                </a>
-            </li>
-            <li className='mb-2'>
-                <a href="#" className='text-link hover:text-white text-sm font-semibold'>
-                    Simay ve ErenYalcn
-                </a>
-            </li>
-            <li className='mb-2'>
-                <a href="#" className='text-link hover:text-white text-sm font-semibold'>
-                    Arabada +200km/h
-                </a>
-            </li>
-            <li className='mb-2'>
-                <a href="#" className='text-link hover:text-white text-sm font-semibold'>
-                    Haluk Levent List
-                </a>
-            </li>
-            <li className='mb-2'>
-                <a href="#" className='text-link hover:text-white text-sm font-semibold'>
-                    Enstrümantal Müzik
-                </a>
-            </li>
-             <li className='mb-2'>
-                <a href="#" className='text-link hover:text-white text-sm font-semibold'>
-                    Hip Hop Amerika
-                </a>
-            </li>
-            <li className='mb-2'>
-                <a href="#" className='text-link hover:text-white text-sm font-semibold'>
-                    3. Çalma Listem
-                </a>
-            </li>
-            <li className='mb-2'>
-                <a href="#" className='text-link hover:text-white text-sm font-semibold'>
-                    Simay ve ErenYalcn
-                </a>
-            </li>
-            <li className='mb-2'>
-                <a href="#" className='text-link hover:text-white text-sm font-semibold'>
-                    Arabada +200km/h
-                </a>
-            </li>
-            <li className='mb-2'>
-                <a href="#" className='text-link hover:text-white text-sm font-semibold'>
-                    Haluk Levent List
-                </a>
-            </li>
-            <li className='mb-2'>
-                <a href="#" className='text-link hover:text-white text-sm font-semibold'>
-                    Enstrümantal Müzik
-                </a>
-            </li>
-             <li className='mb-2'>
-                <a href="#" className='text-link hover:text-white text-sm font-semibold'>
-                    Hip Hop Amerika
-                </a>
-            </li>
-            
-            
-            
-        </ul>
+    <nav className="flex">
+      <ul>
+        <li className="mb-2">
+          <a
+            href="#"
+            className="text-link hover:text-white text-sm font-semibold"
+          >
+            3. Çalma Listem
+          </a>
+        </li>
+        <li className="mb-2">
+          <a
+            href="#"
+            className="text-link hover:text-white text-sm font-semibold"
+          >
+            Haluk ve ErenYalcn
+          </a>
+        </li>
+        <li className="mb-2">
+          <a
+            href="#"
+            className="text-link hover:text-white text-sm font-semibold"
+          >
+            Arabada +200km/h
+          </a>
+        </li>
+        <li className="mb-2">
+          <a
+            href="#"
+            className="text-link hover:text-white text-sm font-semibold"
+          >
+            Haluk Levent List
+          </a>
+        </li>
+        <li className="mb-2">
+          <a
+            href="#"
+            className="text-link hover:text-white text-sm font-semibold"
+          >
+            Enstrümantal Müzik
+          </a>
+        </li>
+        <li className="mb-2">
+          <a
+            href="#"
+            className="text-link hover:text-white text-sm font-semibold"
+          >
+            Hip Hop Amerika
+          </a>
+        </li>
+        <li className="mb-2">
+          <a
+            href="#"
+            className="text-link hover:text-white text-sm font-semibold"
+          >
+            3. Çalma Listem
+          </a>
+        </li>
+        <li className="mb-2">
+          <a
+            href="#"
+            className="text-link hover:text-white text-sm font-semibold"
+          >
+            Haluk ve ErenYalcn
+          </a>
+        </li>
+        <li className="mb-2">
+          <a
+            href="#"
+            className="text-link hover:text-white text-sm font-semibold"
+          >
+            Arabada +200km/h
+          </a>
+        </li>
+        <li className="mb-2">
+          <a
+            href="#"
+            className="text-link hover:text-white text-sm font-semibold"
+          >
+            Haluk Levent List
+          </a>
+        </li>
+        <li className="mb-2">
+          <a
+            href="#"
+            className="text-link hover:text-white text-sm font-semibold"
+          >
+            Enstrümantal Müzik
+          </a>
+        </li>
+        <li className="mb-2">
+          <a
+            href="#"
+            className="text-link hover:text-white text-sm font-semibold"
+          >
+            Hip Hop Amerika
+          </a>
+        </li>
+      </ul>
     </nav>
-
-  )
-  }
+  );
+}
